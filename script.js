ccc8886672f80e21eb0e3482b67e1a165206bc77
Rupali Shahi <rupali.ceinc@gmail.com>
2017-04-17 18:05:49 +0530

    btn action

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
+var dynamicContainer = document.getElementById("dynamic");
 //btn action
 var btn = document.getElementById("btn");
-btn.addEventListener("click")
+btn.addEventListener("click", function(){
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open("GET", "https://shahi-frontend.github.io/api/products.json");
+    ourRequest.onload = function(){
+    //    console.log(ourRequest.responseText);
 
-var ourRequest = new XMLHttpRequest();
-ourRequest.open("GET", "https://shahi-frontend.github.io/api/products.json");
-ourRequest.onload = function(){
-//    console.log(ourRequest.responseText);
+        var ourData = JSON.parse(ourRequest.responseText);
+//        console.log(ourData.products[0]);
+        renderHtml(ourData);
+        
+    };
+
+    //request to server
+    ourRequest.send();
+});
+
+function renderHtml(data){
+    var htmlString = "";
     
-    var ourData = JSON.parse(ourRequest.responseText);
-    console.log(ourData.products[0])
+    for(i=0; i<=data.length; i++){
+        htmlString += "<p>" + data[i].products.Price + "</p>";
+    }
+    dynamicContainer.insertAdjacentHTML("beforeend", htmlString);
 };
-
-//request to server
-ourRequest.send();
