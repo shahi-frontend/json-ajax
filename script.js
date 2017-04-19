3af8ffb8c5b87f8c44e0bc3c2851a650d9a3f369
Rupali Shahi <rupali.ceinc@gmail.com>
2017-04-19 13:24:48 +0530

    Ajax call

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+var pageCounter = 1;
 var dynamicContainer = document.getElementById("dynamic");
 //btn action
 var btn = document.getElementById("btn");
 btn.addEventListener("click", function(){
     var ourRequest = new XMLHttpRequest();
-    ourRequest.open("GET", "https://shahi-frontend.github.io/api/products.json");
+    ourRequest.open("GET", "https://learnwebcode.github.io/json-example/animals-" + pageCounter + ".json");
     ourRequest.onload = function(){
     //    console.log(ourRequest.responseText);
 
@@ -15,14 +16,24 @@ btn.addEventListener("click", function(){
 
     //request to server
     ourRequest.send();
+    pageCounter++;
+    if(pageCounter > 3){
+        btn.classList.add("hide");
+    }
 });
 
 function renderHtml(data){
     var htmlString = "";
-    var c = data.products;
-    for(i=0; i<c.length; i++){
-        console.log(c[i].Price);
-        htmlString += "<p>" + c[i].Price + "</p>";
+//    var c = data.products;
+    for(i=0; i<data.length; i++){
+        console.log(data[i].name);
+        htmlString += "<p>" + data[i].name + " ";
+        
+        for (j=0; j<data[i].foods.likes.length; j++){
+            htmlString += data[i].foods.likes[j];
+        }
+        htmlString += ".</p>";
+        
     }
     dynamicContainer.insertAdjacentHTML("beforeend", htmlString);
 };
